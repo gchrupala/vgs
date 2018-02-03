d906da2e28352aa98e1f1bdf60a5debee609d9df
Grzegorz Chrupala <g.chrupala@uvt.nl>
2018-02-03 13:33:55 +0100

    Deconvolution

--- onion/conv.py
+++ onion/conv.py
@@ -5,10 +5,11 @@ import onion.init as init
 class Convolution1D(nn.Module):
     """A one-dimensional convolutional layer.
     """
-    def __init__(self, size_in, length, size, stride=1):
+    def __init__(self, size_in, length, size, stride=1, padding=None):
         super(Convolution1D, self).__init__()
         util.autoassign(locals())
-        self.Conv = nn.Conv1d(self.size_in, self.size, self.length, stride=self.stride, padding=self.length, bias=False)
+        padding = padding if padding is not None else self.length
+        self.Conv = nn.Conv1d(self.size_in, self.size, self.length, stride=self.stride, padding=padding, bias=False)
         # use Glorot uniform initialization
         self.Conv.weight.data = init.glorot_uniform((self.size, self.size_in, self.length, 1)).squeeze()
         #FIXME what is the correct padding???
@@ -17,3 +18,23 @@ class Convolution1D(nn.Module):
         # signal's shape is (B, T, C) where B=batch size, T=timesteps, C=channels
         out = self.Conv(signal.permute(0, 2, 1))
         return out.permute(0, 2, 1)
+
+
+class ConvTranspose1D(nn.Module):
+    """A one-dimensional convolutional layer.
+    """
+    def __init__(self, size_in, length, size, stride=1, padding=None):
+        super(ConvTranspose1D, self).__init__()
+        util.autoassign(locals())
+        padding = padding if padding is not None else self.length
+        self.ConvT = nn.ConvTranspose1d(self.size_in, self.size, self.length, stride=self.stride, padding=padding, bias=False)
+        # use Glorot uniform initialization
+        self.ConvT.weight.data = init.glorot_uniform((self.size, self.size_in, self.length, 1)).squeeze()
+        #FIXME what is the correct padding???
+
+    def forward(self, signal):
+        # signal's shape is (B, T, C) where B=batch size, T=timesteps, C=channels
+        out = self.ConvT(signal.permute(0, 2, 1))
+        return out.permute(0, 2, 1)
+
+
