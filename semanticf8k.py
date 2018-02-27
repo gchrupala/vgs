7b3ad2d8aab247cf195fdd2d16180d4ab83906e3
Akos Kadar <u1257964@plum.uvt.nl>
2018-02-27 12:50:18 +0100

    MLP init

--- semanticf8k.py
+++ semanticf8k.py
@@ -7,7 +7,87 @@ from sklearn.linear_model import Perceptron
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+from torch import nn
+from torch import optim
 
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_out, dropout=0.5, 
+                 max_epoch=50, batch_size=64, patience=2):
+        """
+        input_dim: int, dimensionality of the input
+        hidden_dim: int, hidden size
+        n_out: int, number of output classes
+        dropout: float, dropout probability
+        max_epoch: int, maximum number of epochs to train
+        batch_size, int, batch size
+        patience: int, stop after patience number of epochs of no improvement
+        """
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.n_out = n_out
+        self.dropout = dropout
+        self.model = nn.Sequential(nn.Linear(self.input_dim, self.hidden_dim),
+                               nn.Dropout(p=self.dropout),
+                               nn.Sigmoid(),
+                               nn.Linear(self.hidden_dim, self.n_out)).cuda()
+        self.patience = patience
+        self.optimizer = optim.Adam(self.model.parameters())
+        self.criterion = nn.MultiLabelSoftMarginLoss()
+        self.all_costs = []
+
+    def gen_split(self, X, y):
+        permutation = np.random.permutation(len(X))
+        X_train, X_test, y_train, y_test = train_test_split(X[permutation], 
+                                                            y[permutation])
+        return X_train, X_test, y_train, y_test
+    
+    def train_epoch(self, X, y, epoch_size=1):
+        self.model.train()
+        losses = []
+        for i in range(0, len(X), self.batch_size):
+            # forward
+            idx = torch.LongTensor(permutation[i:i + self.batch_size]).cuda()
+            Xbatch = Variable(X.index_select(0, idx)).cuda()
+            ybatch = Variable(y.index_select(0, idx)).cuda()
+            output = self.model(Xbatch)
+            # loss
+            loss = self.criterion(output, ybatch)
+            losses.append(loss.data.mean())
+            # backward
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            print(loss)
+        self.nepoch += 1
+        print('[%d/%d] Loss: %.3f' % (self.nepoch, self.max_epochs, np.mean(losses)))  
+    
+    def eval(self, X, y):
+        self.model.eval()
+        costs = []
+        for i in range(0, len(X), self.batch_size):
+            # forward
+            idx = torch.LongTensor(permutation[i:i + self.batch_size]).cuda()
+            Xbatch = Variable(X.index_select(0, idx)).cuda()
+            ybatch = Variable(y.index_select(0, idx)).cuda()
+            output = self.model(Xbatch)
+            # loss
+            loss = self.criterion(output, ybatch)
+            costs.append(loss.data.mean())
+            # backward
+           print(loss)
+        print(np.mean(losses))
+
+    def fit(self, X, y, early_stop=True):
+        self.nepoch = 0
+        best = -2
+        stop_train = False
+        early_stop_count = 0
+        X_train, X_test, y_train, y_test = self.gen_split(X, y)
+        for i in range(self.max_epoch):
+            train_epoch(X_train, y_train)
+
+            
 
 ids, txts, labels = [], [], []
 with open("/roaming/u1257964/semanticf8k/semantic_flickraudio_labels.csv") as csvfile:
