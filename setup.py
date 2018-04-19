ae848aefd319530f3f307ac0d1adf254c28746c5
Grzegorz Chrupala <g.chrupala@uvt.nl>
2018-04-19 11:08:00 +0200

    Add requirements to setup.py file.

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@
 from setuptools import setup
 
 setup(name='vgs',
-      version='0.1',
+      version='0.2',
       description='Visually grounded speech models in Pytorch',
       url='https://github.com/gchrupala/vgs',
       author='Grzegorz Chrupała',
       author_email='g.chrupala@uvt.nl',
       license='MIT',
       packages=['onion','vg'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+          'torch',
+          'torchvision',
+          'sklearn',
+          'scipy'
+      ])
